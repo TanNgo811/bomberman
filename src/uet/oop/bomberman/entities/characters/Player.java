1f228632e377a605ac686956ebe115fb0030ae51
PhTL19 <phatk96lhp@gmail.com>
2020-11-27 10:27:32 +0700

    Collision update

--- src/uet/oop/bomberman/entities/characters/Player.java
+++ src/uet/oop/bomberman/entities/characters/Player.java
@@ -1,53 +1,40 @@
 package uet.oop.bomberman.entities.characters;
 
-import javafx.animation.Transition;
-import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
-import javafx.util.Duration;
-import uet.oop.bomberman.Renderer;
-import uet.oop.bomberman.animations.PlayerAnimation;
-import uet.oop.bomberman.animations.SpriteAnimation;
+import uet.oop.bomberman.GameLoop;
+import uet.oop.bomberman.Sandbox;
 import uet.oop.bomberman.boundedbox.RectBoundedBox;
 import uet.oop.bomberman.entities.AnimatedEntity;
 import uet.oop.bomberman.entities.Direction;
 import uet.oop.bomberman.entities.Entity;
-import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.graphics.Sprite;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Player extends AnimatedEntity {
 
 //    private final String walkRightPath = "file:res/sprites/player_right_";
-    SpriteAnimation currentSprite;
     Direction currentDirection;
     Direction _direction;
     Sprite sprite;
 
-    PlayerAnimation playerAnimations;
-
-    ImageView img = new ImageView();
-
-    RectBoundedBox playerBoundary;
+//    RectBoundedBox playerBoundary;
 
     double reduceBoundarySizePercent=0.45;
     double scale = 1;
-
     public Player(int x, int y, Image img) {
         super( x, y, img);
 //        sprite = Sprite.player_right;
 //        positionX = x;
 //        positionY = y;
 //        _sprite = Sprite.player_right;
-        playerBoundary = new RectBoundedBox(getX()+(int)(16*getReduceBoundarySizePercent()),
-                getY()+(int)(16*getReduceBoundarySizePercent()),
-                (int)(16* getScale())-2*+(int)(16*getReduceBoundarySizePercent()),
-                (int)(16* getScale())-2*+(int)(16*getReduceBoundarySizePercent()));
+//        playerBoundary = new RectBoundedBox(getX()+(int)(16*getReduceBoundarySizePercent()),
+//                getY()+(int)(16*getReduceBoundarySizePercent()),
+//                (int)(16* getScale())-2*+(int)(16*getReduceBoundarySizePercent()),
+//                (int)(16* getScale())-2*+(int)(16*getReduceBoundarySizePercent()));
+//        gc.setStroke(Color.RED);
+//        gc.strokeRect(0,0,16,16);
+        this.boundary = new RectBoundedBox(x+6, y+6, 20, 26);
     }
 
     public double getScale() {
@@ -59,14 +46,6 @@ public class Player extends AnimatedEntity {
         move(1, direction);
     }
 
-
-    private void setCurrentSprite(SpriteAnimation s) {
-//        if (s != null) {
-            currentSprite = s;
-//        } else {
-//            System.out.println("Sprite missing!");
-//        }
-    }
 
 //    private void setCurrentSprite(Sprite s) {
 ////        if (s != null) {
@@ -119,69 +98,72 @@ public class Player extends AnimatedEntity {
         } else {
             switch (direction) {
                 case UP:
-                    this.y -= step;
-//                    _sprite = Sprite.movingSprite(Sprite.player_up_1, Sprite.player_up_2, _animate, 20);
-//                    setCurrentSprite(playerAnimations.getMoveUpSprite());
-                    currentDirection = Direction.UP;
+                    if(checkCollisions(x, y - step)) {
+                        this.y -= step;
+                        this.img = Sprite.movingSprite(Sprite.player_right, Sprite.player_up_1, Sprite.player_up_2, _animate, 20).getFxImage();
+                        currentDirection = Direction.UP;
+                    }
                     break;
 
                 case DOWN:
-                    this.y += step;
-//                    _sprite = Sprite.movingSprite(Sprite.player_down_1, Sprite.player_down_2, _animate, 20);
-//                    setCurrentSprite(playerAnimations.getMoveDownSprite());
-                    currentDirection = Direction.DOWN;
+                    if(checkCollisions(x, y + step)) {
+                        this.y += step;
+                        this.img = Sprite.movingSprite(Sprite.player_down, Sprite.player_down_1, Sprite.player_down_2, _animate, 20).getFxImage();
+                        currentDirection = Direction.DOWN;
+                    }
                     break;
 
                 case RIGHT:
-                    this.x += step;
-//                    _sprite = Sprite.movingSprite(Sprite.player_right_1, Sprite.player_right_2, _animate, 20);
+                    if(checkCollisions(x + step, y)) {
+                        this.x += step;
+                        this.img = Sprite.movingSprite(Sprite.player_right, Sprite.player_right_1, Sprite.player_right_2, _animate, 20).getFxImage();
 
 //                    List<Image> images = new ArrayList<>();
-//
-//                    images.add(Sprite.player_right.getFxImage());
-//                    images.add(Sprite.player_right_1.getFxImage());
-//                    images.add(Sprite.player_right_2.getFxImage());
-//
-//                    Transition animation = new Transition() {
-//                        {
-//                            setCycleDuration(Duration.millis(1000)); // total time for animation
-//                        }
-//
-//                        @Override
-//                        protected void interpolate(double fraction) {
-//
-//                            int index = (int) (fraction*(images.size()-1));
-//                            img.setImage(images.get(index));
-//
-//                        }
-//                    };
-//                    animation.play();
-//
-//                    try {
-//                        Thread.sleep(0);
-//                    } catch (Exception e) {
-//                        System.out.println(e);
-//                    }
-//                    setCurrentSprite(playerAnimations.getMoveRightSprite());
-//                    setCurrentSprite(Sprite.movingSprite(Sprite.player_right_1, Sprite.player_right_2, 0, 20));
 
-                    currentDirection = Direction.RIGHT;
-
+                        currentDirection = Direction.RIGHT;
+                    }
                     break;
 
                 case LEFT:
-                    this.x -= step;
-//                    _sprite = Sprite.movingSprite(Sprite.player_left_1, Sprite.player_left_2, _animate, 20);
+                    if(checkCollisions(x - step, y)) {
+                        this.x -= step;
+                        this.img = Sprite.movingSprite(Sprite.player_left, Sprite.player_left_1, Sprite.player_left_2, _animate, 20).getFxImage();
 //                    setCurrentSprite(playerAnimations.getMoveRightSprite());
-                    currentDirection = Direction.LEFT;
+                        currentDirection = Direction.LEFT;
+                    }
                     break;
 
                 default:
-//                    _sprite = Sprite.player_right;
+                    this.img = Sprite.player_right.getFxImage();
 //                    setCurrentSprite(playerAnimations.getPlayerIdleSprite());
             }
         }
+//        System.out.println("Player: x="+x+" y="+y+" bw="+this.boundary.getBoundary().getWidth()+" bh="+this.boundary.getBoundary().getHeight());
     }
+
+    public boolean isColliding(Entity b){
+        RectBoundedBox otherEntityBoundary = b.getBoundary();
+        return this.boundary.checkCollision(otherEntityBoundary);
+    }
+
+    public boolean checkCollisions(int _x, int _y) {
+        this.boundary.setPosition(_x, _y,0);
+        for (Entity e : GameLoop.stillObjects) {
+            if (e != this && isColliding(e) && !e.canCollide()) {
+                this.boundary.setPosition(x, y,0);
+
+                System.out.println("Player x="+getX()+" y="
+                        +getY()+" colliding with x="+e.getX()
+                        +" y="+e.getY());
+
+                return false;
+            }
+        }
+//        this.boundary.setPosition(x, y,0);
+        System.out.println("no colliding");
+        return true;
+    }
+
 
     @Override
     public void update() {
@@ -191,8 +173,8 @@ public class Player extends AnimatedEntity {
     @Override
     public void render(GraphicsContext gc) {
         super.render(gc);
-//        Renderer.playAnimation(currentSprite);
-
+        gc.strokeRect(this.boundary.getBoundary().getMinX(),this.boundary.getBoundary().getMinY()
+                ,this.boundary.getBoundary().getWidth(),this.boundary.getBoundary().getHeight());
     }
 
     public double getReduceBoundarySizePercent() {
